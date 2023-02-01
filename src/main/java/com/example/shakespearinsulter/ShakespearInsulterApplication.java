ea3eff3c1f47ae50029077f3ee11fcb372726fdb
nilsson46 <nilssonsimon46@gmail.com>
2023-02-01 09:38:29 +0100

    Initial commit

---
+++ src/main/java/com/example/shakespearinsulter/ShakespearInsulterApplication.java
@@ -0,0 +1,13 @@
+package com.example.shakespearinsulter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShakespearInsulterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShakespearInsulterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/shakespearinsulter/ShakespearInsulterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shakespearinsulter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShakespearInsulterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
