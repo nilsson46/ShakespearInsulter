36b767ec96ca91b87d0e741642343af969b6d433
nilsson46 <nilssonsimon46@gmail.com>
2023-02-01 09:54:11 +0100

    Added a insulter model

--- src/main/java/com/example/shakespearinsulter/controller/InsulterController.java
+++ src/main/java/com/example/shakespearinsulter/controller/InsulterController.java
@@ -1,4 +1,5 @@
 package com.example.shakespearinsulter.controller;
 
 public class InsulterController {
+
 }
---
+++ src/main/java/com/example/shakespearinsulter/model/Insulter.java
@@ -0,0 +1,17 @@
+package com.example.shakespearinsulter.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter @Setter
+public class Insulter {
+    private int id;
+    private String name;
+    private String insult;
+    private int rating; 
+
+}
